9a50ba94a0fb56e515022ef2a47ede0c3cab7c64
EmJee <mart-janroeleveld@outlook.com>
2023-02-10 16:09:02 +0100

    Make not-changing strings readonly

--- server/utils/storage.utils.ts
+++ server/utils/storage.utils.ts
@@ -5,8 +5,9 @@ import { ConfigItem, getConfigString } from './config.utils'
 // To set it up locally, follow the following guide:
 // https://cloud.google.com/docs/authentication/provide-credentials-adc
 
-const STORAGE_BUCKET = 'the-cocktail-collective-images'
-export const STORAGE_BUCKET_URL = `https://storage.googleapis.com/${STORAGE_BUCKET}`
+const STORAGE_BUCKET = 'the-cocktail-collective-images' as const
+export const STORAGE_BUCKET_URL =
+	`https://storage.googleapis.com/${STORAGE_BUCKET}` as const
 
 export async function getBucket() {
 	const projectId = await getConfigString(ConfigItem.GoogleCloudProject)
