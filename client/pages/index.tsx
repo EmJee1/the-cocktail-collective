6aab85c22c15886cd88b3cb37d3ed0a9c4218d10
EmJee <mart-janroeleveld@outlook.com>
2023-02-10 11:16:48 +0100

    Add basic card

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -7,7 +7,8 @@ import Grid from '@mui/material/Grid'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
-import { CardActionArea } from '@mui/material'
+import CardActionArea from '@mui/material/CardActionArea'
+import CardMedia from '@mui/material/CardMedia'
 
 interface HomeProps {
 	recipes: DbRecipe[]
@@ -27,23 +28,43 @@ export default function Home({ recipes }: HomeProps) {
 			</Head>
 			<main>
 				<Container>
-					<Grid columns={3} spacing={4}>
+					<Grid container spacing={2}>
 						{recipes.map(recipe => (
-							<Card>
-								<CardActionArea
-									href={`/recipe/${recipe._id}`}
-									component={NextLink}
-								>
-									<CardContent>
-										<Typography gutterBottom variant="h5">
-											{recipe.name}
-										</Typography>
-										{recipe.steps.map(step => (
-											<Typography variant="body1">{step}</Typography>
-										))}
-									</CardContent>
-								</CardActionArea>
-							</Card>
+							<Grid key={recipe._id.toString()} item xs={12} sm={6} md={4}>
+								<Card>
+									<CardActionArea
+										href={`/recipe/${recipe._id}`}
+										component={NextLink}
+									>
+										<CardMedia sx={{ height: 300 }}>
+											<div
+												style={{
+													position: 'relative',
+													width: '100%',
+													height: '100%',
+												}}
+											>
+												<NextImage
+													src="https://placekitten.com/g/400/400"
+													alt=""
+													fill
+													style={{ objectFit: 'cover' }}
+												/>
+											</div>
+										</CardMedia>
+										<CardContent>
+											<Typography gutterBottom variant="h5">
+												{recipe.name}
+											</Typography>
+											{recipe.steps.map((step, i) => (
+												<Typography key={i} variant="body1">
+													{step}
+												</Typography>
+											))}
+										</CardContent>
+									</CardActionArea>
+								</Card>
+							</Grid>
 						))}
 					</Grid>
 				</Container>
