3c45cb1ad4a8499c603e2e27449a350f958fdb85
EmJee <mart-janroeleveld@outlook.com>
2023-02-28 19:36:42 +0100

    Load environment file from the correct env

--- server/utils/dotenv.utils.ts
+++ server/utils/dotenv.utils.ts
@@ -2,18 +2,21 @@ import { join as joinPath } from 'node:path'
 import { readFile } from 'node:fs/promises'
 import { DotenvParseOutput, parse as parseDotenv } from 'dotenv'
 import { Secret } from './secrets.utils'
+import { Environment, getEnvironment } from './environment.utils'
 
 export async function getSecret(configItem: Secret) {
-	const config = await getConfig()
+	const config = await getConfig(getEnvironment())
 	return config[configItem]
 }
 
 let config: DotenvParseOutput
 
-async function getConfig() {
+async function getConfig(environment: Environment) {
 	if (!config) {
-		const envFile = joinPath(__dirname, '..', '.env')
-		const envFileContents = await readFile(envFile)
+		const envFilename =
+			environment === Environment.Test ? '.env.test' : '.env.local'
+		const envFilepath = joinPath(__dirname, '..', envFilename)
+		const envFileContents = await readFile(envFilepath)
 		config = parseDotenv(envFileContents)
 	}
 
