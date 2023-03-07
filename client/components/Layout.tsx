62ef9052295771bfde628aedff6db73597510468
EmJee <mart-janroeleveld@outlook.com>
2023-03-07 11:44:31 +0100

    Remove unused notifications button

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -53,14 +53,6 @@ export default function Layout({ children }: { children: ReactNode }) {
 								</div>
 								<div className="hidden md:block">
 									<div className="ml-4 flex items-center md:ml-6">
-										<button
-											type="button"
-											className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-										>
-											<span className="sr-only">View notifications</span>
-											<BellIcon className="h-6 w-6" aria-hidden="true" />
-										</button>
-
 										{/* Profile dropdown */}
 										<Menu as="div" className="relative ml-3">
 											<div>
@@ -146,13 +138,6 @@ export default function Layout({ children }: { children: ReactNode }) {
 											{user.email}
 										</div>
 									</div>
-									<button
-										type="button"
-										className="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-									>
-										<span className="sr-only">View notifications</span>
-										<BellIcon className="h-6 w-6" aria-hidden="true" />
-									</button>
 								</div>
 								<div className="mt-3 space-y-1 px-2">
 									{userNavigation.map(item => (
