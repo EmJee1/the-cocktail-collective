3c84b7f40086945404b02e6491b8ec360c80c427
EmJee <mart-janroeleveld@outlook.com>
2023-03-13 18:27:16 +0100

    Make storage bucket publicly accessible

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,8 +4,15 @@ provider "google" {
   region = "europe-west4"
 }
 
-resource "google_storage_bucket" "the-cocktail-collective-images" {
+resource "google_storage_bucket" "images" {
   location = "europe-west4"
   name     = "the-cocktail-collective-images"
   storage_class = "STANDARD"
+}
+
+resource "google_storage_bucket_iam_member" "member" {
+  provider = google
+  bucket   = google_storage_bucket.images.name
+  role     = "roles/storage.objectViewer"
+  member   = "allUsers"
 }
